e7f9c0b1945bd10d1801ebaf353c2cf29ccb2ee7
Anurag Maurya <mauryaanurag375@gmail.com>
2023-06-17 20:30:07 +0530

    update stuff

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -3,7 +3,51 @@ import "./Add.scss"
 const Add = () => {
   return (
     <div>
-      
+      <div className="add">
+        <div className="container">
+          <h1>Add New Gig</h1>
+          <div className="section">
+            <div className="left">
+              <label htmlFor="">Title</label>
+              <input type="text" placeholder="e.g. I will do something I'm really code at" />
+              <label htmlFor="">Category</label>
+              <select name="cats" id="cats">
+                <option value="">Design</option>
+                <option value="">Web Development</option>
+                <option value="">Animation</option>
+                <option value="">Music</option>
+                <option value="">Writer</option>
+              </select>
+              <label htmlFor="">Cover Image</label>
+              <input type="file" />
+              <label htmlFor="">Upload Images</label>
+              <input type="file" multiple/>
+              <label htmlFor="">Description</label>
+              <textarea name="" id="" cols="30" rows="16" placeholder='Brief descriptions to introduce your services to customers'></textarea>
+              <button>Create</button>
+            </div>
+            <div className="right">
+              <label htmlFor="">Service Title</label>
+              <input type="text" placeholder='e.g. One page web design'/>
+              <label htmlFor="">Short Descriptions</label>
+              <textarea name="" id="" cols="30" rows="10" placeholder='short description of the services you provide'></textarea>
+              <label htmlFor="">Delivery Time (e.g 3 days)</label>
+              <input type="number" min={1} />
+              <label htmlFor="">Revision Numbers</label>
+              <input type="number" min={1} />
+              <label htmlFor="">Add Features</label>
+              <input type="text" placeholder='e.g page design' />
+              <input type="text" placeholder='e.g. file uploading' />
+              <input type="text" placeholder='e.g setting up domain' />
+              <input type="text" placeholder='e.g hosting' />
+              <input type="text" placeholder='' />
+              <label htmlFor="">Price</label>
+              <input type="number" min={1} />
+              
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,9 +1,90 @@
 import React from 'react'
 import './Message.scss'
+import { Link } from 'react-router-dom'
 const Message = () => {
   return (
     <div>
-      
+      <div className="message">
+        <div className="container">
+          <span className="breadcrumbs">
+            <Link to='/messages'>MESSAGES </Link>{'>'} JOHN DOE
+          </span>
+          <div className="messages">
+            <div className="item">
+              <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at hendrerit nunc. Integer a lacus eget mi sollicitudin efficitur eu vel felis. Phasellus tempus eu elit vel tempus. Vestibulum blandit ullamcorper orci, vel egestas sapien gravida quis. Duis tincidunt hendrerit diam non venenatis. Mauris quis odio sed nunc suscipit lobortis. Aliquam turpis neque, malesuada id felis sed, dignissim vestibulum tellus. Nullam porta eu ex a eleifend. Curabitur quis consequat ex. .
+              </p>
+            </div>
+            <div className="item owner">
+              <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at hendrerit nunc. Integer a lacus eget mi sollicitudin efficitur eu vel felis. Phasellus tempus eu elit vel tempus. Vestibulum blandit ullamcorper orci, vel egestas sapien gravida quis. Duis tincidunt hendrerit diam non venenatis. Mauris quis odio sed nunc suscipit lobortis. Aliquam turpis neque, malesuada id felis sed, dignissim vestibulum tellus. Nullam porta eu ex a eleifend. Curabitur quis consequat ex. .
+              </p>
+            </div>
+            <div className="item">
+              <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at hendrerit nunc. Integer a lacus eget mi sollicitudin efficitur eu vel felis. Phasellus tempus eu elit vel tempus. Vestibulum blandit ullamcorper orci, vel egestas sapien gravida quis. Duis tincidunt hendrerit diam non venenatis. Mauris quis odio sed nunc suscipit lobortis. Aliquam turpis neque, malesuada id felis sed, dignissim vestibulum tellus. Nullam porta eu ex a eleifend. Curabitur quis consequat ex. .
+              </p>
+            </div>
+            <div className="item owner">
+              <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at hendrerit nunc. Integer a lacus eget mi sollicitudin efficitur eu vel felis. Phasellus tempus eu elit vel tempus. Vestibulum blandit ullamcorper orci, vel egestas sapien gravida quis. Duis tincidunt hendrerit diam non venenatis. Mauris quis odio sed nunc suscipit lobortis. Aliquam turpis neque, malesuada id felis sed, dignissim vestibulum tellus. Nullam porta eu ex a eleifend. Curabitur quis consequat ex. .
+              </p>
+            </div>
+            <div className="item">
+              <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at hendrerit nunc. Integer a lacus eget mi sollicitudin efficitur eu vel felis. Phasellus tempus eu elit vel tempus. Vestibulum blandit ullamcorper orci, vel egestas sapien gravida quis. Duis tincidunt hendrerit diam non venenatis. Mauris quis odio sed nunc suscipit lobortis. Aliquam turpis neque, malesuada id felis sed, dignissim vestibulum tellus. Nullam porta eu ex a eleifend. Curabitur quis consequat ex. .
+              </p>
+            </div>
+            <div className="item owner">
+              <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at hendrerit nunc. Integer a lacus eget mi sollicitudin efficitur eu vel felis. Phasellus tempus eu elit vel tempus. Vestibulum blandit ullamcorper orci, vel egestas sapien gravida quis. Duis tincidunt hendrerit diam non venenatis. Mauris quis odio sed nunc suscipit lobortis. Aliquam turpis neque, malesuada id felis sed, dignissim vestibulum tellus. Nullam porta eu ex a eleifend. Curabitur quis consequat ex. .
+              </p>
+            </div>
+            <div className="item">
+              <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at hendrerit nunc. Integer a lacus eget mi sollicitudin efficitur eu vel felis. Phasellus tempus eu elit vel tempus. Vestibulum blandit ullamcorper orci, vel egestas sapien gravida quis. Duis tincidunt hendrerit diam non venenatis. Mauris quis odio sed nunc suscipit lobortis. Aliquam turpis neque, malesuada id felis sed, dignissim vestibulum tellus. Nullam porta eu ex a eleifend. Curabitur quis consequat ex. .
+              </p>
+            </div>
+            <div className="item owner">
+              <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at hendrerit nunc. Integer a lacus eget mi sollicitudin efficitur eu vel felis. Phasellus tempus eu elit vel tempus. Vestibulum blandit ullamcorper orci, vel egestas sapien gravida quis. Duis tincidunt hendrerit diam non venenatis. Mauris quis odio sed nunc suscipit lobortis. Aliquam turpis neque, malesuada id felis sed, dignissim vestibulum tellus. Nullam porta eu ex a eleifend. Curabitur quis consequat ex. .
+              </p>
+            </div>
+            <div className="item">
+              <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at hendrerit nunc. Integer a lacus eget mi sollicitudin efficitur eu vel felis. Phasellus tempus eu elit vel tempus. Vestibulum blandit ullamcorper orci, vel egestas sapien gravida quis. Duis tincidunt hendrerit diam non venenatis. Mauris quis odio sed nunc suscipit lobortis. Aliquam turpis neque, malesuada id felis sed, dignissim vestibulum tellus. Nullam porta eu ex a eleifend. Curabitur quis consequat ex. .
+              </p>
+            </div>
+            <div className="item">
+              <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at hendrerit nunc. Integer a lacus eget mi sollicitudin efficitur eu vel felis. Phasellus tempus eu elit vel tempus. Vestibulum blandit ullamcorper orci, vel egestas sapien gravida quis. Duis tincidunt hendrerit diam non venenatis. Mauris quis odio sed nunc suscipit lobortis. Aliquam turpis neque, malesuada id felis sed, dignissim vestibulum tellus. Nullam porta eu ex a eleifend. Curabitur quis consequat ex. .
+              </p>
+            </div>
+            <div className="item owner">
+              <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                Loremaai ipsum dolor sit amet, consectetur adipiscing elit. Morbi at hendrerit nunc. Integer a lacus eget mi sollicitudin efficitur eu vel felis. Phasellus tempus eu elit vel tempus. Vestibulum blandit ullamcorper orci, vel egestas sapien gravida quis. Duis tincidunt hendrerit diam non venenatis. Mauris quis odio sed nunc suscipit lobortis. Aliquam turpis neque, malesuada id felis sed, dignissim vestibulum tellus. Nullam porta eu ex a eleifend. Curabitur quis consequat ex. .
+              </p>
+            </div>  
+            
+          </div>
+
+          <div className="write">
+            <textarea name="" placeholder='write a message' id="" cols="30" rows="10"></textarea>
+            <button>Send</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
